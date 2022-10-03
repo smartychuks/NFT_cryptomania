d412fe9381908f4e1346ae4519d429f17f4afc08
smartychuks <Smirhty@gmail.com>
2022-10-03 08:55:00 +0100

    first commit

---
+++ hardhat-tutorial/my-app/pages/api/[tokenId].js
@@ -0,0 +1,16 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+
+  const tokenId = req.query.tokenId;
+  
+  const name = `Crypto Mania #${tokenId}`;
+  const description = "Crypto Mania is NFT collection to Onboard web2 people to web3";
+  const image = `https://raw.githubusercontent.com/smartychuks/debugNFT/master/hardhat-tutorial/my-app/public/cryptomania/${Number({tokenId) - 1}.svg`;
+
+  return res.json({
+    name: name,
+    description: description,
+    image: image,
+  });
+}
